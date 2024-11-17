bb6c3b44bdaf869719b63efa5f4d1f1f9f3bc529
kinin01 <kiningumbuvi@gmail.com>
2024-11-17 15:53:44 +0000

    added shipping info

--- a_stripe/urls.py
+++ a_stripe/urls.py
@@ -4,9 +4,10 @@ from .views import *
 urlpatterns = [
     path('', shop_view, name="shop"),
     path('product/<product_id', product_view, name="product"),
-    #path('payment_successful/', payment_successful, name="payment_successful"),
-    #path('payment_cancelled/', payment_cancelled, name="payment_cancelled"),
-    #path ('stripe_webhook/', stripe_webhook, name = 'stripe_webhook'),
+    path('payment_successful/', payment_successful, name="payment_successful"),
+    path('payment_cancelled/', payment_cancelled, name="payment_cancelled"),
+    path ('stripe_webhook/', stripe_webhook, name = 'stripe_webhook'),
     path ('add_to_cart/<product_id>', add_to_cart, name = 'add_to_cart'),
+    path ('checkout/', checkout_view, name = 'checkout'),
     
 ]
--- a_stripe/views.py
+++ a_stripe/views.py
@@ -3,10 +3,12 @@ import stripe
 from django.conf import settings
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import *
 from .utils import *
 from .cart import Cart
+from .forms import *
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -71,3 +73,83 @@ def remove_from_cart(request, product_id):
     cart = Cart(request)
     cart.remove(product_id)
     return redirect('cart')
+
+
+@login_required
+def checkout_view(request):
+    shipping_info = ShippingInfo.objects.filter(user=request.user).first()
+    
+    if shipping_info:
+        form = ShippingForm(instance=shipping_info)
+    else:
+        form = ShippingForm(initial={'email': request.user.email})
+    
+    if request.method == 'POST':
+        form = ShippingForm(request.POST, instance=shipping_info)
+        if form.is_valid():
+            shipping_info = form.save(commit=False)
+            shipping_info.user = request.user
+            shipping_info.email = form.cleaned_data['email'].lower()
+            shipping_info.save()
+            
+            cart = Cart(request)
+            checkout_session = create_checkout_session(cart, shipping_info.email)
+            
+            CheckoutSession.objects.create(
+                checkout_id = checkout_session.id,
+                shipping_info = shipping_info,
+                total_cost = cart.get_total_cost()
+            )
+            
+            return redirect(checkout_session.url, code=303)
+    
+    return render(request, 'a_stripe/checkout.html', {'form': form})
+
+
+
+def payment_successful(request):
+    checkout_session_id = request.GET.get('session_id', None)
+    
+    if checkout_session_id:
+        session = stripe.checkout.Session.retrieve(checkout_session_id)
+        customer_id = session.customer
+        customer = stripe.Customer.retrieve(customer_id)
+        
+        if settings.CART_SESSION_ID in request.session:
+            del request.session[settings.CART_SESSION_ID]
+            
+        if settings.DEBUG:
+            checkout = CheckoutSession.objects.get(checkout_id=checkout_session_id)
+            checkout.has_paid = True
+            checkout.save()
+    
+    return render(request, 'a_stripe/payment_successful.html', {'customer': customer})
+
+
+def payment_cancelled(request):
+    return render(request, 'a_stripe/payment_cancelled.html')
+
+
+@require_POST
+@csrf_exempt
+def stripe_webhook(request):
+    endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
+    payload = request.body
+    signature_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+    
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, signature_header, endpoint_secret
+        )
+    except:
+        return HttpResponse(status=400)
+    
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        checkout_session_id = session.get('id')
+        checkout = CheckoutSession.objects.get(checkout_id=checkout_session_id)
+        checkout.has_paid = True
+        checkout.save()
+        
+    return HttpResponse(status=200)
